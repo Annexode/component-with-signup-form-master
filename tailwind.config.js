f86d55b1784719244793b837b91916fb6c900a99
Annexode <annexode@gmail.com>
2024-09-09 19:46:28 +0200

    Sign up form styled with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./output/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(0, 100%, 74%)",
+        secondary: "hsl(154, 59%, 51%)",
+        ubururu: "hsl(248, 32%, 49%)",
+      },
+      fontSize: {
+        eshanu: "0.5rem",
+        esheshatu: "0.6rem",
+      },
+      borderWidth: {
+        imwe: "1px",
+      },
+      width: {
+        ubugari: "960px",
+      },
+      height: {
+        uburebure: "885px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
